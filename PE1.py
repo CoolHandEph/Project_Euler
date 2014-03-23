72974419d476530c13181b4e07557e466b229b66
CoolHandEph <luke.m.breckenridge@gmail.com>
2014-03-23 19:25:39 -0400

    Update PE1.py

--- PE1.py
+++ PE1.py
@@ -1,5 +1,5 @@
 import math
-n = range(1000)
+n = range(1,1001)
 value = 0
 for i in n:
 	if int(i % 15) == 3 or int(i % 15) == 5 or int(i % 15) == 6 or int(i % 15) == 9 or int(i % 15) == 10 or int(i % 15) == 12 or int(i % 15) == 0:
